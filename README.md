30e80f19220415e8442431255fe44c7dcbe7b3d7
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2018-04-20 15:03:09 +0200

    update the code with timeout support and re-license it under MPL-2.0

---
+++ README.md
@@ -0,0 +1,18 @@
+## About
+
+__testerbender__ is a test runner useful as a gatekeeper between a test repository and a live one in a continuous integration setup.
+
+If you're putting the "testerbender/" dir under Git control, add "testerbender/log" to ".gitignore".
+
+The log timestamps are in UTC.
+
+## License
+
+MPL-2.0
+
+## Credits
+
+- author: Ștefan Talpalaru <stefantalpalaru@yahoo.com>
+
+- homepage: https://github.com/stefantalpalaru/testerbender
+
--- testerbender/testerbender.py
+++ testerbender/testerbender.py
@@ -1,37 +1,21 @@
 #!/usr/bin/env python
+# coding: utf-8
 
-# Copyright (C) 2010  Odeon Consulting Group LLC 
+# Copyright © 2010-2018 - Ștefan Talpalaru <stefantalpalaru@yahoo.com> */
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-import os
+from email.mime.text import MIMEText
+import csv
+import datetime
 import logging
-import atexit
+import os
+import signal
+import smtplib
 import subprocess
 import sys
-from pprint import pprint
-import csv
-import smtplib
-from email.mime.text import MIMEText
-import urllib2, urllib
-try:
-    import cPickle as pickle
-except:
-    import pickle
-import datetime
 import time
-import re
 
 
 DIR = os.path.abspath(os.path.dirname(__file__))
@@ -58,7 +42,6 @@ def read_data():
     for row in csvreader:
         DATA[row[0]] = row[1]
     datafile.close()
-    #pprint(DATA)
 
 def write_data():
     datafile = open(DATA_FILE, 'w')
@@ -80,17 +63,10 @@ formatter = UTCFormatter('[%(asctime)s] %(message)s', '%d/%b/%Y:%H:%M:%S')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-# log the termination
-#def stopping():
-    #logger.info('stopped')
-#atexit.register(stopping)
-
 # commit info
 os.chdir(REPO_DIR)
 output = subprocess.Popen(['git', 'log', '--topo-order', '--format=format:%H|%an', '-n', '1'], stdout=subprocess.PIPE).communicate()[0]
 commit, author = output.strip().split('|')
-#pprint(commit)
-#pprint(author)
 
 # email
 def send_email(subject, body):
@@ -114,49 +90,26 @@ def log_normal_commit(commit, author):
         logger.info('normal commit: %s' % commit)
         logger.info('normal commit author: %s' % author)
 
-def api_call(data):
-    try:
-        f = urllib2.urlopen(STATS_SERVER_URL, urllib.urlencode(data))
-    except urllib2.HTTPError, e:
-        print e.code
-        print e.msg
-        print e.headers
-        print e.fp.read()
-        return False
-
-    response = pickle.loads(f.read())
-    if not response['success']:
-        print 'log uploading failed: %s' % response['msg']
-        return False
-    else:
-        return response
-
-def upload_logs():
-    if not len(API_KEY):
-        return
-    response = api_call({'key': API_KEY, 'action': 'get_last_date'})
-    if response:
-        # upload only the new log entries
-        new_entries = []
-        for line in open(LOG_FILE, 'r'):
-            match = re.search(r'^\[([^\]]+)\] ([^:]+): (.+)$', line)
-            if match:
-                entry_date = datetime.datetime.strptime(match.groups()[0], '%d/%b/%Y:%H:%M:%S')
-                if entry_date > response['last_date']:
-                    new_entries.append(line)
-        if new_entries:
-            #print ''.join(new_entries)
-            response = api_call({'key': API_KEY, 'action': 'upload', 'new_entries': ''.join(new_entries)})
-
 def main():
     # running the test
     exit_code = 0 # used by the post-update hook
-    #logger.info('started')
     os.chdir(TEST_DIR)
     for test_cmd in TEST_CMDS:
         test_cmd_str = ' '.join(test_cmd)
         print test_cmd_str
-        p = subprocess.Popen(test_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        start_time = datetime.datetime.now()
+        p = subprocess.Popen(test_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        while p.poll() is None:
+            time.sleep(1)
+            if (datetime.datetime.now() - start_time).seconds > TIMEOUT:
+                # the test timed out
+                exit_code = 1
+                print 'Time out - the test took longer than %d seconds.' % TIMEOUT
+                os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+                time.sleep(5)
+                if p.returncode is None:
+                    os.killpg(os.getpgid(p.pid), signal.SIGKILL)
+                break
         output = p.communicate()[0]
         if p.returncode != 0:
             # the test failed
@@ -207,7 +160,6 @@ def main():
         log_normal_commit(commit, author)
     DATA['last_tested_commit'] = commit
     write_data()
-    upload_logs()
 
     sys.exit(exit_code)
 
